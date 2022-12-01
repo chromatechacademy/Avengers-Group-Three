31f48c834860b1393b943c4a8e33dbe67bd1e12d
Hayk <mikhayk@yahoo.com>
2022-11-30 22:13:52 -0500

    minor changes-1

--- src/test/java/com/chroma/stepDefinitions/IncomeModuleStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/IncomeModuleStepDef.java
@@ -1,10 +1,8 @@
 package com.chroma.stepDefinitions;
 
-import org.junit.Assert;
 import com.chroma.pages.DashboardPage;
 import com.chroma.utils.CucumberLogUtils;
 import com.chroma.web.CommonUtils;
-
 import cucumber.api.java.en.Then;
 
 public class IncomeModuleStepDef {
@@ -19,6 +17,13 @@ public class IncomeModuleStepDef {
     public void user_has_to_see_folowing_links(String addIncome, String searchIncome, String incomeHead) {
         String actualAddIncomeText = DashboardPage.dynamicXpathForSubModule(addIncome).getText();
         CommonUtils.assertEquals(addIncome, actualAddIncomeText);
+
+        String actualSearchIncomeText = DashboardPage.dynamicXpathForSubModule(searchIncome).getText();
+        CommonUtils.assertEquals(searchIncome, actualSearchIncomeText);
+
+        String actualIncomeHeadText = DashboardPage.dynamicXpathForSubModule(incomeHead).getText();
+        CommonUtils.assertEquals(incomeHead, actualIncomeHeadText);
+
         CucumberLogUtils.logExtentScreenshot();
         CucumberLogUtils.logScreenShot();
 
