807ab1c2d2db92d61c7c4bc43242b8d4e0bf5f55
Anastasiia Tutuieva <AnastasiiaTutuieva@chromatechacademy.net>
2022-12-01 13:00:30 -0500

    ..

--- src/test/java/com/chroma/stepDefinitions/StudentInformationModuleStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/StudentInformationModuleStepDef.java
@@ -2,6 +2,7 @@ package com.chroma.stepDefinitions;
 
 import com.chroma.appsCommon.PageInitializer;
 import com.chroma.stepsImplementation.StudentInformationModulSpetsImpl;
+import com.chroma.web.CommonUtils;
 import com.chroma.web.WebDriverUtils;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -12,14 +13,12 @@ public class StudentInformationModuleStepDef extends PageInitializer {
     @Given("user is on CTSMS home page {string}")
     public void user_is_on_CTSMS_home_page(String url) {
         WebDriverUtils.driver.get(url);
-
     }
 
     @When("user clicks on {string}")
     public void user_clicks_on(String string) throws InterruptedException {
-       dashboardPage.studentInfoBtn.click();
-         Thread.sleep(2000);
-
+        dashboardPage.studentInfoBtn.click();
+        CommonUtils.sleep(3000);    
     }
 
     @Then("user is able to see links displayed {string}, {string}, {string}, {string}, {string}, {string}, {string}")
@@ -27,8 +26,8 @@ public class StudentInformationModuleStepDef extends PageInitializer {
             String disabledStudents, String bulkDelete, String studentCategories, String studentHouse,
             String disableReason) {
 
-       StudentInformationModulSpetsImpl.AssertStudentInfoModule(studentDetails, studentAdmission, disabledStudents, bulkDelete, studentCategories, studentHouse, disableReason);
-
+        StudentInformationModulSpetsImpl.AssertStudentInfoModule(studentDetails, studentAdmission, disabledStudents,
+                bulkDelete, studentCategories, studentHouse, disableReason);
     }
 
 }
