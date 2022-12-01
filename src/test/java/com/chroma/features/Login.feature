afa0ebe8e5adce90df9dbd5c5c8a325f880c1829
Mari H <marih.@Maris-MacBook-Pro.local>
2022-12-01 12:20:54 -0500

    automated user story VNGRS-2

--- src/test/java/com/chroma/features/Login.feature
+++ src/test/java/com/chroma/features/Login.feature
@@ -1,6 +1,6 @@
 Feature: Automating login page
 
-@Valeriia @Regression @Progression @VNGRS-1
+@Valeriia @Regression @VNGRS-1
 Scenario: Testing login page
 Given a user is on the CTSMS login page "https://chroma.mexil.it/site/login"
 When user logs with valid username "general@teacher.com" and password "123456"
@@ -8,5 +8,6 @@ Then user is navigated to the dashboard page
 
 @Progression @Mari @VNGRS-2
 Scenario: Invalid Credentials login
-When user logs with invalid username "general@teacher.co" and password "12346"
+Given a user is on the CTSMS login page "https://chroma.mexil.it/site/login"
+When user logs with valid username "general@teacher.co" and password "12346"
 Then message "Invalid Username or Password" displays
--- src/test/java/com/chroma/stepDefinitions/LoginStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/LoginStepDef.java
@@ -2,27 +2,22 @@ package com.chroma.stepDefinitions;
 
 import java.io.IOException;
 import org.junit.Assert;
-import com.chroma.pages.DashboardPage;
-import com.chroma.pages.LoginPage;
 import com.chroma.stepsImplementation.LoginStepsImpl;
 import com.chroma.utils.CucumberLogUtils;
 import com.chroma.web.CommonUtils;
 import com.chroma.web.WebDriverUtils;
+import com.chroma.appsCommon.PageInitializer;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 // Add step definitions in this class
 // NOTE: Every step definitions class Extends PageInitializer
 
-public class LoginStepDef {
-
-    public LoginPage loginPage = new LoginPage();
-    public DashboardPage dashboardPage = new DashboardPage();
+public class LoginStepDef extends PageInitializer {
 
     @Given("a user is on the CTSMS login page {string}")
     public void a_user_is_on_the_CTSMS_login_page(String url) {
         WebDriverUtils.driver.get(url);
-
     }
 
     @When("user logs with valid username {string} and password {string}")
@@ -31,18 +26,12 @@ public class LoginStepDef {
         LoginStepsImpl.login(username, password);
     }
 
-        @When("user logs with valid username {string} and password {string}")
-        public void user_logs_with_invalid_username_and_password(String username, String password) throws IOException {
-            LoginStepsImpl.login(username, password);
-    }
-
     @Then("user is navigated to the dashboard page")
     public void user_is_navigated_to_the_dashboard_page() {
         boolean isChromaTextDisplayed = dashboardPage.dashBoardChromaTechText.isDisplayed();
         Assert.assertTrue(isChromaTextDisplayed);
         CucumberLogUtils.logScreenShot();
         CucumberLogUtils.logExtentScreenshot();
-
     }
 
     @Then("message {string} displays")
@@ -51,8 +40,5 @@ public class LoginStepDef {
         CommonUtils.assertEquals(invalidLoginMessage, actualInvalidLoginMessage);
         CucumberLogUtils.logScreenShot();
         CucumberLogUtils.logExtentScreenshot();
-
-    
-
     }
 }
