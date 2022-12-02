1eabd3f6b89110baaad2b7b6a3f1ec52ea1ea00f
DComanici <danicomanici86@gmail.com>
2022-12-01 23:24:28 -0500

    Student Admission no spaces

--- src/test/java/com/chroma/stepDefinitions/AcademicsStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/AcademicsStepDef.java
@@ -10,10 +10,7 @@ public class AcademicsStepDef extends PageInitializer {
     public void user_is_able_to_see_submodules_displayed(String classTimetableLink, String teachersTimetablelink,
             String assignClassTeacherlink, String promoteStudentsLink,
             String subjectGroupLink, String subjectsLink, String classLink, String sectionsLink) {
-
                 AcademicsModuleStepsImpl.AssertAcademicsSubModules(classTimetableLink, teachersTimetablelink, assignClassTeacherlink, 
                 promoteStudentsLink, subjectGroupLink, subjectsLink, classLink, sectionsLink);
-
-
     }
 }
