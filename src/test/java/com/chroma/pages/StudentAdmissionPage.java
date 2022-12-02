a006f2354e847aa13b613c4166c10787cd7b127e
DComanici <danicomanici86@gmail.com>
2022-12-02 14:03:33 -0500

    Student Admissions fixed dates

--- src/test/java/com/chroma/pages/StudentAdmissionPage.java
+++ src/test/java/com/chroma/pages/StudentAdmissionPage.java
@@ -8,65 +8,77 @@ import com.chroma.appsCommon.PageInitializer;
 import com.chroma.web.WebDriverUtils;
 
 public class StudentAdmissionPage extends PageInitializer {
+
+    //Locator for Admission No
     @FindBy(xpath = "//input[@name='admission_no']")
     public static WebElement admissionNoTextBox;
 
+    //Locator for Roll Number
     @FindBy(xpath = "//input[@name='roll_no']")
     public static WebElement rollNumberTextBox;
 
+    //Locator for First Name text box
     @FindBy(xpath = "//input[@name='firstname']")
     public static WebElement firstNameTextBox;
 
+    //Locator for lirst Name text box
     @FindBy(xpath = "//input[@name='lastname']")
     public static WebElement lastNameTextBox;
 
+    //Locator for Class dropdown
     @FindBy(xpath = "//select[@name='class_id']")
     public static WebElement classDropdown;
 
+    //Locator for Section dropdown
     @FindBy(xpath = "//select[@name='section_id']")
     public static WebElement sectionDropdown;
 
+    //Locator for Gender dropdown
     @FindBy(xpath = "//select[@name='gender']")
     public static WebElement genderDropdown;
 
+    //Locator for Category dropdown
     @FindBy(xpath = "//select[@name='category_id']")
     public static WebElement categoryDropdown;
 
+    //Locator for Blood Group dropdown
     @FindBy(xpath = "//select[@name='blood_group']")
     public static WebElement bloodGroupDropdown;
 
+    //Locator for Email text box
     @FindBy(xpath = "//input[@name='email']")
     public static WebElement emailTextBox;
 
+    //Locator for Date of Birth box
     @FindBy(xpath = "//input[@name='dob']")
     public static WebElement dateOfBirthBox;
 
+    //Locator for Admission Date box
     @FindBy(xpath = "//*[@id='admission_date']")
     public static WebElement admissionDateBox;
 
-    @FindBy(xpath = "//td[normalize-space()='9']")
-    public static WebElement admissionDateDay;
-
+    //Locator for As on Date box
     @FindBy(xpath = "//input[@name='measure_date']")
-    public static WebElement asOfDateBox;
+    public static WebElement asOnDateBox;
 
-    @FindBy(xpath = "//body/div/div/section/div/div/div/form[@name='employeeform']/div/div[1]/div[1]/div[1]")
-    public static WebElement keepDate;
-
+    //Locator for Guardian Email text box
     @FindBy(xpath = "//input[@name='guardian_email']")
     public static WebElement guardianEmail;
 
+    //Locator for Guardian Address text box
     @FindBy(xpath = "//textarea[@name='guardian_address']")
     public static WebElement guardianAddress;
 
+    //Locator for save button
     @FindBy(xpath = "//button[@type='submit'][normalize-space()='Save']")
     public static WebElement saveButton;
 
+    //Locator for Record Saved Message
     @FindBy(xpath = "//div[normalize-space()='Record Saved Successfully']")
     public static WebElement recordSavedMessage;
 
     /***
-     * Use this method to access Mobile No, Height, Weight
+     * Use this method to locate Mobile No, Height, Weight
      * @param customTextbox
      * @return
      */
@@ -76,7 +88,7 @@ public class StudentAdmissionPage extends PageInitializer {
     }
     
     /**
-     * Use this method to access any text box in the guardian parent detail section
+     * Use this method to locate any text box in the guardian parent detail section
      * @param boxLabel
      * @return
      */
@@ -86,7 +98,7 @@ public class StudentAdmissionPage extends PageInitializer {
     }
     
     /**
-     * Use this method to choose your guardian
+     * Use this method to locate radio button in front of desired guardian
      * @param guardian
      * @return
      */
@@ -94,16 +106,6 @@ public class StudentAdmissionPage extends PageInitializer {
         return WebDriverUtils.driver.findElement(By.xpath("//input[@value='" + guardian + "']"));
     }
     
-    /**
-     * Use this method to locate a day
-     * @param day
-     * @return
-     */
-    public static WebElement dynamicXpathForAdmissionDay(String day) {
-        return WebDriverUtils.driver.findElement(By.xpath("//td[normalize-space()='"+day+"']"));
-    }
-
-
     public StudentAdmissionPage() {
         PageFactory.initElements(WebDriverUtils.driver, this);
     }
